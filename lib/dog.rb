f65516162868308a23bce340b95b90cae185127b
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-07 17:08:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,6 +2,12 @@ class Dog
   attr_reader :name
   attr_accessor :owner, :mood
   
+  @@all = []
+  
+  def self.all 
+    @@all 
+  end 
+  
   def initialize(name, owner)
     @name = name 
     @owner = owner 
