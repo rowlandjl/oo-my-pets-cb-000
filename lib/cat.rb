ea6a88009c6e93ae1a8d10f8a5f8d31f49f70ded
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-07 17:41:03 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,18 @@
 class Cat
-  # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name 
+  
+  @@all = []
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def initialize(name, owner)
+    @name = name 
+    @owner = owner 
+    @mood = "nervous"
+    self.class.all << self 
+  end 
+  
 end
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
-  # code goes here
+  attr_reader :name
+  attr_accessor :owner, :mood
+  
+  @@all = []
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def initialize(name, owner)
+    @name = name 
+    @owner = owner 
+    @mood = "nervous"
+    self.class.all << self 
+  end 
+  
 end
